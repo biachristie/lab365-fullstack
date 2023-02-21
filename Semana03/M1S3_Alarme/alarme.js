553d9ad19dc0dd111dc134c2d90d383afaf802e8
biachristie <beatrizchristie@yahoo.com.br>
2023-02-21 15:41:07 -0300

    add alarme.css and new functions at alarme.js

--- Semana03/M1S3_Alarme/alarme.js
+++ Semana03/M1S3_Alarme/alarme.js
@@ -1,13 +1,30 @@
-const d1 = '2023-03-19';
-const d2 = '2023-02-19';
+let idInterval = 0;
+document.querySelector("#inputButton").addEventListener("click", calculateDays);
 
-setInterval(() => {
-    const differenceInMs = new Date(d1) - new Date(d2);
-    const differenceInDays = differenceInMs / (1000 * 60 * 60 * 24);
-    
-    if (d1 === d2) {
-        console.log("Feliz Aniversário!!!");
-    } else {
-        console.log(`Faltam ${differenceInDays} dias para o aniversário.`);
-    }
-}, 5000);
+function calculateDays() {
+    if (idInterval !== 0) { stopAlarm(); }
+
+    const birthday = new Date(document.querySelector("#inputBirthday").value);
+    const today = new Date();
+
+    const differenceInMs = birthday - today;
+    const differenceInDays = Math.ceil(differenceInMs / (1000 * 60 * 60 * 24));
+
+    displayAlarm(differenceInDays);
+}
+
+function displayAlarm(differenceInDays) {
+    idInterval = setInterval(() => {
+        if (differenceInDays < 0) {
+            document.querySelector("#alarmText").innerText = `Seu aniversário já passou.`;
+        } else if (differenceInDays === 0) {
+            document.querySelector("#alarmText").innerText = `🎉🎉 Feliz aniversário!!! 🎉🎉`;
+        } else if (differenceInDays === 1) {
+            document.querySelector("#alarmText").innerText = `Falta ${differenceInDays} dia para seu aniversário.`;
+        } else if (differenceInDays > 1) {
+            document.querySelector("#alarmText").innerText = `Faltam ${differenceInDays} dias para o aniversário!`;
+        }
+    }, 1000);
+}
+
+function stopAlarm() { clearInterval(idInterval); }

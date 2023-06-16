bbf2096464b4e5a51e83c8a9cbe2b97a9cb48b4a
biachristie <beatrizchristie@yahoo.com.br>
2023-06-16 12:35:42 -0300

    add Main component

---
+++ Modulo02/Semana06/babyhug/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Card from '../Card/Card'
+
+function Main() {
+    return(
+        <React.Fragment>
+            <Card />
+        </React.Fragment>
+    )
+}
+
+export default Main

b4f4d4893198106711d1b2c8bad59d4974e44797
biachristie <beatrizchristie@yahoo.com.br>
2023-06-23 17:44:37 -0300

    transfer user info to Card component; render users info

--- Modulo02/Semana07/my-project/src/App.jsx
+++ Modulo02/Semana07/my-project/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from "./components/Header/Header"
 import Form from "./components/Form/Form"
 import Card from "./components/Card/Card"
 
 function App() {
+  const [users, setUsers] = useState([])
+
   return (
     <React.Fragment>
       <Header />
-      <Form />
-      <Card />
+      <Form setUsers={ setUsers } />
+      <Card users={ users } />
     </React.Fragment>
   )
 }
--- Modulo02/Semana07/my-project/src/components/Card/Card.jsx
+++ Modulo02/Semana07/my-project/src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
-
-function Card() {
+function Card({ users }) {
+    const renderList = (user) => {
+        return <ul key={ user.nickname }>
+            <li>Nickname: { user.nickname }</li>
+            <li>Age: { user.age }</li>
+            <li>E-mail: { user.email }</li>
+        </ul>
+    }
+    
     return (
-        <>
-
-        </>
+        <div className="card-container">
+            { users.map(renderList) }
+        </div>
     );
 }
 

dc3dc55b7646e6f9cae31f6aa2d9ac59d03674e3
biachristie <beatrizchristie@yahoo.com.br>
2023-06-25 16:40:28 -0300

    add pages routes

--- Modulo02/Semana06/babyhug/src/App.jsx
+++ Modulo02/Semana06/babyhug/src/App.jsx
@@ -1,18 +1,24 @@
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import Header from './components/Header/Header'
-import Banner from './components/Banner/Banner'
-import Main from './components/Main/Main'
-import Footer from './components/Footer/Footer'
+import { HomePage } from './pages/HomePage/HomePage';
+import { ContactPage } from './pages/ContactPage/ContactPage';
+import { ProductPage } from './pages/ProductPage/ProductPage';
+import { FAQPage } from './pages/FAQPage/FAQPage';
+import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+
+import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <Header title='BabyHug' />
-      <Banner title='nossos produtos' subtitle='escolha o melhor para você' />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <HomePage /> }/>
+        <Route path='product' element={ <ProductPage /> }/>
+        <Route path='contact' element={ <ContactPage /> }/>
+        <Route path='faq' element={ <FAQPage /> }/>
+        <Route path='*' element={ <NotFoundPage /> }/>
+      </Routes>
+    </Router>
   )
 }
 

b1ee21c10b027842f9cf5bd3bac5243a33062c4a
biachristie <beatrizchristie@yahoo.com.br>
2023-06-25 16:43:39 -0300

    add products info to ProductPage

---
+++ Modulo02/Semana06/babyhug/src/pages/ProductPage/ProductPage.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
+import Card from '../../components/Card/Card'
+import Footer from '../../components/Footer/Footer'
+
+export const ProductPage = () => {
+    return (
+        <div className='container'>
+            <Header title='BabyHug' />
+            <Banner title='nossos produtos' subtitle='escolha o melhor para você' />
+            <div className="card-container">
+                <Card />
+            </div>
+            <Footer />
+        </div>
+    )
+}

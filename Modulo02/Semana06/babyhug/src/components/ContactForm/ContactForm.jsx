02d6ab228a3e8b6a62eb07e5fce0b17711463208
biachristie <beatrizchristie@yahoo.com.br>
2023-07-05 18:55:07 -0300

    add handleInput and handleSubmit functions to FormComponent

--- Modulo02/Semana06/babyhug/src/components/ContactForm/ContactForm.jsx
+++ Modulo02/Semana06/babyhug/src/components/ContactForm/ContactForm.jsx
@@ -1,44 +1,80 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+
 import './ContactForm.css'
 
 function ContactForm() {
+    const [data, setData] = useState({
+        name: '',
+        telephone: '',
+        email: '',
+        message: ''
+    })
+    const [content, setContent] = useState(0)
+
+    const { setUser, usersList, setUsersList } = useContext(UserContext)
+
+    const handleInput = (e) => {
+        const { value, id } = e.target
+        setData({ ...data, [id]: value })
+        
+        if (id === 'message') { setContent(value.length) }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setUser(data)
+        setUsersList([ ...usersList, data ])
+        console.log(usersList);
+    }
+
     return (
         <div className="contactForm-container">
             <h1 className="contactForm-title">Deixe o seu recado</h1>
-            <form className="contactForm-form">
+            <form className="contactForm-form" onSubmit={ handleSubmit }>
                 <div>
                     <fieldset className="contactForm-field">
-                        <label className='contactForm-label' htmlFor="">Nome</label>
+                        <label className='contactForm-label' htmlFor="name">Nome</label>
                         <input 
                             type="text"
                             placeholder="Digite seu nome..."
-                            id="input-name"
+                            id="name"
+                            onChange={ handleInput }
                         />
                     </fieldset>
                     <fieldset className="contactForm-field">
-                        <label className='contactForm-label' htmlFor="input-tel">Telefone</label>
+                        <label className='contactForm-label' htmlFor="telephone">Telefone</label>
                         <input 
                             type="tel"
                             placeholder="Digite seu telefone..."
-                            id="input-tel"
+                            id="telephone"
+                            onChange={ handleInput }
                         />
                     </fieldset>
                 </div>
                 <fieldset className="contactForm-field">
-                    <label className='contactForm-label' htmlFor="">E-mail</label>
+                    <label className='contactForm-label' htmlFor="email">E-mail</label>
                     <input
                         type="email"
                         placeholder="Digite seu email..."
-                        id="input-email"
+                        id="email"
+                        onChange={ handleInput }
                     />
                 </fieldset>
                 <fieldset className="contactForm-field">
-                    <label className='contactForm-label' htmlFor="">Mensagem</label>
+                    <label className='contactForm-label' htmlFor="message">Mensagem</label>
                     <textarea 
                         rows={ 6 }
+                        maxLength={ 500 }
                         placeholder="Digite sua mensagem..."
-                        id="input-msg"
+                        id="message"
+                        onChange={ handleInput }
                     />
                 </fieldset>
+                <div id="character-counter">
+                    <span>{ content } / 500 caracteres</span>
+                </div>
                 <button type="submit">Enviar</button>
             </form>
         </div>

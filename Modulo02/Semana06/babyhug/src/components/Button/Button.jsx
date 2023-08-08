fad3513c054cef833df13d296cffbabc632c7517
biachristie <beatrizchristie@yahoo.com.br>
2023-08-08 17:16:54 -0300

    add disabled prop

--- Modulo02/Semana06/babyhug/src/components/Button/Button.jsx
+++ Modulo02/Semana06/babyhug/src/components/Button/Button.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import './Button.css'
 
-function Button({ type, onClick, value, className }) {
+function Button({ type, onClick, value, className, disabled }) {
     return (
         <React.Fragment>
             <button 
                 type={ type } 
                 onClick={ onClick } 
                 className={ className }
+                disabled={ disabled }
             >
                 { value }
             </button>

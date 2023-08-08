e814e81bcc15d38b3cedfe5ea0fce3904e92fb12
biachristie <beatrizchristie@yahoo.com.br>
2023-08-08 18:19:44 -0300

    change modal component to StyledModal

--- Modulo02/Semana06/babyhug/src/components/ProductModal/ProductModal.jsx
+++ Modulo02/Semana06/babyhug/src/components/ProductModal/ProductModal.jsx
@@ -1,9 +1,99 @@
 import { useContext } from 'react';
+import styled from 'styled-components'
 
 import { ProductModalContext } from "../../contexts/ProductModalContext";
 import Button from '../Button/Button';
 
-import "./ProductModal.css"
+const Modal = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ModalContainer = styled.div`
+    width: 550px;
+    border-radius: 10px;
+    background-color: #fff;
+    color: var(--color-porcelain);
+`
+
+const ModalTitle = styled.span`
+    display: inline-block;
+    width: 100%;
+    color: #535353;
+    font-size: 1.8rem;
+    font-weight: 600;
+    text-align: center;
+    border-bottom: 1px solid var(--color-porcelain);
+    padding: 10px 0;
+    margin: 20px 0;
+`
+
+const ModalHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 25px 10px;
+`
+
+const ModalBody = styled.div`
+    margin: 0 25px;
+`
+
+const ModalBodyImg = styled.img`
+    height: auto;
+    width: 500px;
+    border-radius: 10px;
+    margin: auto;
+    margin-bottom: 10px;
+`
+
+const ModalBodyP = styled.p`
+    text-align: justify;
+`
+
+const InfoList = styled.div`
+    margin-top: 15px;
+
+    && li::before {
+        content: '●';
+        color: var(--color-mint);
+        font-size: 25px;
+        line-height: 20px;
+        padding-right: 10px;
+        margin-left: 15px;
+    }
+`
+
+const SpecList = styled.div`
+    margin-top: 15px;
+
+    && li::before {
+        content: '●';
+        color: var(--color-mint);
+        font-size: 25px;
+        line-height: 20px;
+        padding-right: 10px;
+        margin-left: 15px;
+    }
+`
+
+const ModalBtns = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    border-top: 1px solid var(--color-porcelain);
+    padding-top: 20px;
+    margin: 20px 25px;
+    gap: 10px;
+`
 
 function ProductModal() {
     const { setShow, data, setData } = useContext(ProductModalContext)
@@ -22,18 +112,18 @@ function ProductModal() {
     }
 
     return (
-        <div className="productModal">
-            <div className="productModal-container">
-                <span className="productModal-title">Detalhes do Produto</span>
-                <div className="productModal-header">
+        <Modal className="productModal">
+            <ModalContainer className="productModal-container">
+                <ModalTitle className="productModal-title">Detalhes do Produto</ModalTitle>
+                <ModalHeader className="productModal-header">
                     <h2 className="productModal-product">{ data.productName || 'titulo' }</h2>
                     <span className="productModal-price" >R$ { data.price || '9999.99' }</span>
-                </div>
-                <div className="productModal-body">
-                    <img src={ data.photo } alt={ data.photoDescription || 'Lorem ipsum'} />
-                    <p>{ data.productDescription || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, modi facilis nemo aperiam nobis porro asperiores fuga obcaecati atque sed id ratione perspiciatis quidem exercitationem, aspernatur odio sapiente illum corporis.' }</p>
+                </ModalHeader>
+                <ModalBody className="productModal-body">
+                    <ModalBodyImg src={ data.photo } alt={ data.photoDescription || 'Lorem ipsum'} />
+                    <ModalBodyP>{ data.productDescription || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, modi facilis nemo aperiam nobis porro asperiores fuga obcaecati atque sed id ratione perspiciatis quidem exercitationem, aspernatur odio sapiente illum corporis.' }</ModalBodyP>
                     <div className="productModal-lists">
-                        <div className="info-list">
+                        <InfoList className="info-list">
                             <h4>Informações</h4>
                             <ul>
                                 <li>{ data.productInfoList[0] || 'lista' }</li>
@@ -41,8 +131,8 @@ function ProductModal() {
                                 <li>{ data.productInfoList[2] || 'lista' }</li>
                                 <li>{ data.productInfoList[3] || 'lista' }</li>
                             </ul>
-                        </div>
-                        <div className="spec-list">
+                        </InfoList>
+                        <SpecList className="spec-list">
                             <h4>Ficha Técnica</h4>
                             <ul>
                                 <li>{ data.productSpecList[0] || 'lista' }</li>
@@ -50,17 +140,15 @@ function ProductModal() {
                                 <li>{ data.productSpecList[2] || 'lista' }</li>
                                 <li>{ data.productSpecList[3] || 'lista' }</li>
                             </ul>
-                        </div>
+                        </SpecList>
                     </div>
-                </div>
-                <div className="productModal-btns">
+                </ModalBody>
+                <ModalBtns className="productModal-btns">
                     <Button onClick={ handleCloseModal } value="Fechar" />
                     <Button value="Comprar" />
-                    {/* <button onClick={ handleCloseModal }>Fechar</button>
-                    <button>Comprar</button> */}
-                </div>
-            </div>
-        </div>
+                </ModalBtns>
+            </ModalContainer>
+        </Modal>
     );
 }
 

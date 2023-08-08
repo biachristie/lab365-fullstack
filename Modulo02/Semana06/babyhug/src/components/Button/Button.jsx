c7c78bf8e713c09b49fd33d8ebb4f531a5692b69
biachristie <beatrizchristie@yahoo.com.br>
2023-08-08 17:47:08 -0300

    change button component to StyledButton

--- Modulo02/Semana06/babyhug/src/components/Button/Button.jsx
+++ Modulo02/Semana06/babyhug/src/components/Button/Button.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-import './Button.css'
+const StyledButton = styled.button`
+    color: var(--color-snow);
+    font-size: 0.9rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    background-color: var(--color-mint);
+    border: none;
+    border-radius: 10px;
+    padding: 13px 20px 11px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: var(--color-mint-dark);
+    }
+
+    &:disabled {
+        background-color: var(--color-porcelain);
+        cursor: not-allowed;
+    }
+`
 
 function Button({ type, onClick, value, className, disabled }) {
     return (
-        <React.Fragment>
-            <button 
-                type={ type } 
-                onClick={ onClick } 
-                className={ className }
-                disabled={ disabled }
-            >
-                { value }
-            </button>
-        </React.Fragment>
+        <StyledButton
+            type={ type } 
+            onClick={ onClick } 
+            className={ className }
+            disabled={ disabled }
+        >
+            { value }
+        </StyledButton>
     );
 }
 

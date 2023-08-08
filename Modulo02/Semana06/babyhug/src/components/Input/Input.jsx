42a24c9e095424b2995ff27b8c8d5ec022915a70
biachristie <beatrizchristie@yahoo.com.br>
2023-08-08 17:48:35 -0300

    change input component to StyledInput

--- Modulo02/Semana06/babyhug/src/components/Input/Input.jsx
+++ Modulo02/Semana06/babyhug/src/components/Input/Input.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-import './Input.css'
+const StyledInput = styled.input`
+    color: #5a5a5a;
+    border: 1px solid #dbdbdb;
+    border-radius: 7px;
+    padding-left: 15px;
+    outline: none;
+`
 
 function Input({ type, placeholder, id, onChange, className }) {
     return (
-        <React.Fragment>
-            <input 
-                type={ type }
-                placeholder={ placeholder }
-                id={ id }
-                onChange={ onChange }
-                className={ className }
-            />
-        </React.Fragment>
+        <StyledInput
+            type={ type }
+            placeholder={ placeholder }
+            id={ id }
+            onChange={ onChange }
+            className={ className }
+        />
     );
+}
+
+Input.propTypes = {
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    id: PropTypes.string,
+    onChange: PropTypes.func,
+    className: PropTypes.string,
 }
 
 export default Input;

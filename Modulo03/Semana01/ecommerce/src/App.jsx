80880400198cf9accac582992f8b28265168ebe6
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 16:34:06 -0300

    add cart page

--- Modulo03/Semana01/ecommerce/src/App.jsx
+++ Modulo03/Semana01/ecommerce/src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import ProductPage from './pages/Product/Product.page'
+import ProductPage from "./pages/Product/Product.page"
+import CartPage from "./pages/Cart/Cart.page"
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path='/' element={ <HomePage /> } /> */}
-        <Route path='/produtos' element={ <ProductPage /> } />
-        {/* <Route path='/carrinho' element={ <CartPage /> } />
-        <Route path='/checkout' element={ <CheckOutPage /> } /> */}
-        <Route path='/*' element={ <><p>Página não encontrada</p></> } />
+        {/* <Route path="/" element={ <HomePage /> } /> */}
+        <Route path="/produtos" element={ <ProductPage /> } />
+        <Route path="/carrinho" element={ <CartPage /> } />
+        {/* <Route path="/checkout" element={ <CheckOutPage /> } /> */}
+        <Route path="/*" element={ <><p>Página não encontrada</p></> } />
       </Routes>
     </Router>
   )

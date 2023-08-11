9dc7ced618f15d68f912cce2d753a1a6ece50c86
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 09:11:24 -0300

    add pages routes

--- Modulo03/Semana01/ecommerce/src/App.jsx
+++ Modulo03/Semana01/ecommerce/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import ProductPage from './pages/Product/Product.page'
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={ <HomePage /> } /> */}
+        <Route path='/produtos' element={ <ProductPage /> } />
+        {/* <Route path='/carrinho' element={ <CartPage /> } />
+        <Route path='/checkout' element={ <CheckOutPage /> } /> */}
+        <Route path='/*' element={ <><p>Página não encontrada</p></> } />
+      </Routes>
+    </Router>
   )
 }
 

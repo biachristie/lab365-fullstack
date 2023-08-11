acc3afb18b0b44c0dac6ead9a0d262759ac454be
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 11:35:27 -0300

    add onClick to product card component

--- Modulo03/Semana01/ecommerce/src/components/ProductCard/ProductCard.component.jsx
+++ Modulo03/Semana01/ecommerce/src/components/ProductCard/ProductCard.component.jsx
@@ -1,55 +1,67 @@
-import * as React from 'react'
-import AspectRatio from '@mui/joy/AspectRatio'
-import Button from '@mui/joy/Button'
-import Card from '@mui/joy/Card'
-import CardContent from '@mui/joy/CardContent'
-import CardOverflow from '@mui/joy/CardOverflow'
-import Link from '@mui/joy/Link'
-import Typography from '@mui/joy/Typography'
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward'
+import * as React from "react"
+import AspectRatio from "@mui/joy/AspectRatio"
+import Button from "@mui/joy/Button"
+import Card from "@mui/joy/Card"
+import CardContent from "@mui/joy/CardContent"
+import CardOverflow from "@mui/joy/CardOverflow"
+import Link from "@mui/joy/Link"
+import Typography from "@mui/joy/Typography"
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward"
 
-export default function ProductCard({ productImage, productCategory, productName, productCurrency, productPrice }) {
-    return (
-        <Card sx={{ width: 200, maxWidth: '100%', boxShadow: 'lg' }}>
-        <CardOverflow>
-            <AspectRatio sx={{ minWidth: 200 }}>
-            <img
-                src={ productImage }
-                loading="lazy"
-                alt=""
-            />
-            </AspectRatio>
-        </CardOverflow>
-        <CardContent>
-            <Typography 
-                level="body-xs"
-                sx={{ textTransform: 'capitalize' }}
-            >
-                { productCategory }
-            </Typography>
-            <Link
-                href="#product-card"
-                fontWeight="md"
-                color="neutral"
-                textColor="text.primary"
-                overlay
-                endDecorator={ <ArrowOutwardIcon /> }
-            >
-                { productName }
-            </Link>
+export default function ProductCard({ 
+    productImage, 
+    productCategory,
+    productName, 
+    productCurrency, 
+    productPrice, 
+    onClick
+}) {
+        return (
+            <Card sx={{ width: 200, maxWidth: "100%", boxShadow: "lg" }}>
+            <CardOverflow>
+                <AspectRatio sx={{ minWidth: 200 }}>
+                <img
+                    src={ productImage }
+                    loading="lazy"
+                    alt=""
+                />
+                </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+                <Typography 
+                    level="body-xs"
+                    sx={{ textTransform: "capitalize" }}
+                >
+                    { productCategory }
+                </Typography>
+                <Link
+                    href="#product-card"
+                    fontWeight="md"
+                    color="neutral"
+                    textColor="text.primary"
+                    overlay
+                    endDecorator={ <ArrowOutwardIcon /> }
+                >
+                    { productName }
+                </Link>
 
-            <Typography
-                level="title-lg"
-                sx={{ mt: 1, fontWeight: 'xl' }}
-            >
-                { productCurrency} { productPrice }
-            </Typography>
-        </CardContent>
-        <CardOverflow>
-            <Button variant="solid" color="danger" size="lg">
-                Adicione ao carrinho
-            </Button>
-        </CardOverflow>
-        </Card>
-    )
+                <Typography
+                    level="title-lg"
+                    sx={{ mt: 1, fontWeight: "xl" }}
+                >
+                    { productCurrency} { productPrice }
+                </Typography>
+            </CardContent>
+            <CardOverflow>
+                <Button 
+                    variant="solid" 
+                    color="danger" 
+                    size="lg"
+                    onClick={ onClick }
+                >
+                    Adicione ao carrinho
+                </Button>
+            </CardOverflow>
+            </Card>
+        )
 }

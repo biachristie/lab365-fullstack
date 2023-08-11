ff69bf94e2b7a02efc8c8981866b1add260bfc5a
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 16:32:58 -0300

    add totalCartItems to localStorage

--- Modulo03/Semana01/ecommerce/src/context/Cart/Cart.context.jsx
+++ Modulo03/Semana01/ecommerce/src/context/Cart/Cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import PropTypes from "prop-types"
 
 export const CartContext = createContext({
@@ -11,6 +11,16 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState([])
     const [totalCartItems, setTotalCartItems] = useState(0)
+
+    const storedTotalItems = localStorage.getItem('totalCartItems')
+    
+    useEffect(() => {
+        if (storedTotalItems) { setTotalCartItems(Number(storedTotalItems)) }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('totalCartItems', JSON.stringify(totalCartItems))
+    }, [totalCartItems])
 
     return(
         <CartContext.Provider value={{ cartItems, setCartItems, totalCartItems, setTotalCartItems }}>

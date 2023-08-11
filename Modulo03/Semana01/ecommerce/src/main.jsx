2a545f1d9e885069366cd0c6763a73f994a5ced4
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 11:34:50 -0300

    add cart provider to main

--- Modulo03/Semana01/ecommerce/src/main.jsx
+++ Modulo03/Semana01/ecommerce/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { CartProvider } from './context/Cart/Cart.context'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
 )

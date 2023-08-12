f23f32b22519a90531aae29513f69a9cd9426571
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 22:24:29 -0300

    add search provider to main

--- Modulo03/Semana01/ecommerce/src/main.jsx
+++ Modulo03/Semana01/ecommerce/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { SearchProvider } from './context/Search/Search.context.jsx'
 import { CartProvider } from './context/Cart/Cart.context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <SearchProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </SearchProvider>
   </React.StrictMode>,
 )

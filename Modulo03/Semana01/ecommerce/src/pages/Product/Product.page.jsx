d74714d9c7920dd5de9aadbf4bdbe11a13a503ec
biachristie <beatrizchristie@yahoo.com.br>
2023-08-11 22:25:57 -0300

    add filter to products card

--- Modulo03/Semana01/ecommerce/src/pages/Product/Product.page.jsx
+++ Modulo03/Semana01/ecommerce/src/pages/Product/Product.page.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react"
 import Header from "../../components/Header/Header.component"
 import ProductCard from "../../components/ProductCard/ProductCard.component"
 import { ProductService } from "../../services/Product/Product.service"
+import { SearchContext } from "../../context/Search/Search.context"
 import { CartContext } from "../../context/Cart/Cart.context"
 
 import "./Product.page.css"
@@ -13,6 +14,18 @@ function ProductPage() {
     useEffect(() => {
         ProductService.Get().then(result => setProducts(result.products))
     }, [])
+
+    const { searchedTerm } = useContext(SearchContext)
+
+    const searchTerm = (value) => {
+        if (!searchedTerm) {
+            return value
+        } else if (value.title.toLowerCase().includes(searchedTerm.toString().toLowerCase())) {
+            return value
+        } else if (value.brand.toLowerCase().includes(searchedTerm.toString().toLowerCase())) {
+            return value
+        }
+    }
     
     const renderProductCard = (product) => {
         return (
@@ -62,7 +75,10 @@ function ProductPage() {
         <>
             <Header />
             <section className="products-cards">
-                { products && products.map(renderProductCard) }
+                { products && 
+                    products
+                    .filter(searchTerm)
+                    .map(renderProductCard) }
             </section>
         </>
     )

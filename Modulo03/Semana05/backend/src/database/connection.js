9fb17cea0941aa5e385705c9352d2828ca2fb144
biachristie <beatrizchristie@yahoo.com.br>
2023-09-04 17:03:33 -0300

    delete semana 05

--- Modulo03/Semana05/backend/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize')
-const databaseconfig = require('../config/database.config')
-
-const connection = new Sequelize(databaseconfig)
-
-module.exports = { connection }
